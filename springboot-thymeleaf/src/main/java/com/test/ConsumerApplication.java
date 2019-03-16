a8dec0742a7a9b3db4d29f514fa0fdbb1329ebe3
luo2018 <694815891@qq.com>
2019-03-16 22:14:30 +0800

    output by json string

--- springboot-thymeleaf/src/main/java/com/test/ConsumerApplication.java
+++ springboot-thymeleaf/src/main/java/com/test/ConsumerApplication.java
@@ -2,9 +2,10 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableTransactionManagement
+@EnableTransactionManagement(proxyTargetClass = true)
 @SpringBootApplication
 public class ConsumerApplication {
 	public static void main(String[] args) {
--- springboot-thymeleaf/src/main/java/com/test/control/UserControl.java
+++ springboot-thymeleaf/src/main/java/com/test/control/UserControl.java
@@ -1,16 +1,16 @@
 package com.test.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.test.base.BaseController;
 import com.test.domain.User;
-import com.test.service.UserService;
 import com.test.service.impl.UserServiceImpl;
+import com.test.util.ResultUtil;
 
 @RestController
-public class UserControl {
+public class UserControl extends BaseController {
 	@Autowired
 	private UserServiceImpl userService;
 
@@ -26,5 +26,19 @@ public class UserControl {
 		user.setPassword("888");
 		userService.insertUser(user);
 		int a = 1 / 0;
+	}
+
+	@RequestMapping("/getUserListToJsonString")
+	public String getUserListToJsonString() {
+		return ResultUtil.getJsonString(userService.getUserList());
+	}
+    
+	/**
+	 * extends BaseController类，输出字符串
+	 */
+	@RequestMapping("/getUserListToPrintJson")
+	public void getUserListToPrintJson() {
+
+		this.printJson(ResultUtil.getJsonString(userService.getUserList()));
 	}
 }
